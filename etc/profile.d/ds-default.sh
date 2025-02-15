d6c83e0341980ed6d44aab5c08b353ab7e9ebf62
Danny Sung <danny@dannysung.com>
2025-02-15 05:09:52 -0800

    Add some useful aliases

--- etc/profile.d/ds-default.sh
+++ etc/profile.d/ds-default.sh
@@ -13,4 +13,11 @@ export LANG="C"
 # Default file creation mask
 umask 002
 
+# If vim is available, alias vi to vim
 which vim >/dev/null 2>&1  && alias vi >/dev/null 2>&1 || alias vi=vim
+
+# Some useful aliases
+alias ls="ls -GP"
+alias ll="ls -lFa"
+alias pd=pushd
+alias b='cd "${OLDPWD}"'
